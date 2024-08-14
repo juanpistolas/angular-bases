3f690ddb85649c7a2d7d8f00deaf6b00ff1a30c3
JUANIXC <betancourtjuandaniel76@gmail.com>
2024-08-14 12:02:43 -0600

    Fin desección 4

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { elementAt } from 'rxjs';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+
+  public name: string = 'iroman';
+  public age:  number = 45;
+
+  get capitalizedName():string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+
+    return `${ this.name } - ${ this.age }`;
+
+  }
+
+  changeHero():void{
+    this.name = 'Spiderman';
+  }
+  changeAge() {
+    this.age = 25;
+  }
+  resetForm():void {
+    this.name = 'iroman';
+    this.age = 45;
+   // document.querySelectorAll('h1')!.forEach(element => {
+   //   element.innerHTML = '<h1> Desde Angular</h1>';
+   // });
+  }
+}
